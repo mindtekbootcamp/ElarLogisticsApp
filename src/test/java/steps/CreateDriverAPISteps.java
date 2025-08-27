c15a1963f0f3322bbf47863029d02074a77191a6
mindtekbatch14 <training@mindtek.io>
2025-08-27 15:27:32 -0500

    Latest changes

--- src/test/java/steps/CreateDriverAPISteps.java
+++ src/test/java/steps/CreateDriverAPISteps.java
@@ -1,35 +1,66 @@
 package steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
+import pojos.CreateDriverRequest;
 import utilities.ConfigReader;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class CreateDriverAPISteps {
 
-    @Given("user sends post api call with data")
-    public void user_sends_post_api_call_with_data() {
+    Response postResponse;
 
-        Response postResponse = given().baseUri(ConfigReader.getProperty("ElarAPIBaseURL"))
+    @Given("user sends post api call with data")
+    public void user_sends_post_api_call_with_data(DataTable dataTable) {
+
+        Map<String, Object> data=dataTable.asMap(String.class, Object.class);
+
+        CreateDriverRequest createDriverRequest=new CreateDriverRequest();
+        createDriverRequest.setDefaultValues();
+        createDriverRequest.setFull_name(data.get("full_name").toString());
+
+        postResponse = given().baseUri(ConfigReader.getProperty("ElarAPIBaseURL"))
                 .and().header("Accept", "application/json")
                 .and().header("Content-Type", "application/json")
-                .and().header("Cookie","Access=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHVkZW50QG1pbmR0ZWsiLCJoZWFkZXIiOnsidHlwZSI6IkFjY2VzcyIsImFsZyI6IkhTMjU2In0sImV4cCI6MTc1NjMyNTA3MH0.MpcsRReAOh9392Y_SsZaQyJm0WduoRKUa8n576dIv2E; Refresh=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHVkZW50QG1pbmR0ZWsiLCJoZWFkZXIiOnsidHlwZSI6IlJlZnJlc2giLCJhbGciOiJIUzI1NiJ9LCJleHAiOjE3NTYzMjUwNzB9.br_24UDGcsvZpzHNCySndkrjy0RDDEFzuq-Beq2-HDk")
-                .and().body("") // POJO -> Json = SERIALIZATION
+                .and().header("Cookie","Access=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHVkZW50QG1pbmR0ZWsiLCJoZWFkZXIiOnsidHlwZSI6IkFjY2VzcyIsImFsZyI6IkhTMjU2In0sImV4cCI6MTc1NjQxMTUyNX0.XEkhSSy9Lw0MYfJDcFWEp_1kt7fXr3QfZW1MzbW_l_8; Refresh=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHVkZW50QG1pbmR0ZWsiLCJoZWFkZXIiOnsidHlwZSI6IlJlZnJlc2giLCJhbGciOiJIUzI1NiJ9LCJleHAiOjE3NTY0MTE1MjV9.MJI55UZgt_SRaA4WDWzXCnzRyVjn9xdlXwDeuv6NRI0")
+                .and().body(createDriverRequest) // POJO -> Json = SERIALIZATION
                 .and().log().all()
                 .when().post("/drivers");
         postResponse.then().log().all();
-        postResponse.then().statusCode(200);
-
     }
-    @Then("user validates status code {int}")
-    public void user_validates_status_code() {
 
+    @Then("user validates status code {int}")
+    public void user_validates_status_code(int statusCode) {
+        postResponse.then().statusCode(statusCode);
     }
     @Then("user validates response data matches with request data")
     public void user_validates_response_data_matches_with_request_data() {
-
+        // Get createDriverRequest and validate against postResponse body.
+        // Convert postResponse body to Java Object - Deserialization
+        /* Create Java Object for response body:
+         {
+            "uuid": "dc0aee63-305f-4f3c-b9cb-787bc550757e",
+            "id": 2456,
+            "schedule": [],
+            "full_name": "Johny Doe",
+            "is_staff": false,
+            "status": "Off duty",
+            "alerts": [],
+            "created_at": "2025-08-27T20:19:01.225779Z",
+            "updated_at": "2025-08-27T20:19:01.225779Z",
+            "driver_local_identifier": "None",
+            "contacts_phone": [],
+            "contacts_email": [],
+            "contacts_viber": [],
+            "contacts_other": []
+        }
+         */
+        // Validate full_name and is_staff from request object matches with response java object
     }
 
     @Then("user validates response body error message {string}")
