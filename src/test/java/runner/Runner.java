0bbbe61399647822e9440ab2f99295fdcbf10573
dselzer93 <decodaselzer@gmail.com>
2025-09-02 18:35:37 -0500

    test

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
         plugin = {"rerun:target/rerun.txt", "json:target/cucumber.json"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@addDriver",
-        dryRun = false
+        tags = "@searchDriver",
+        dryRun = true
 )
 
 public class Runner {
