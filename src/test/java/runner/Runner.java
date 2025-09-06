ba27a773d3d1f7f0e3bdff0447611ecab523658c
Mindtek <75808354+mindtekbootcamp@users.noreply.github.com>
2025-09-06 10:18:48 -0500

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -9,11 +9,12 @@ import org.junit.runner.RunWith;
         plugin = {"rerun:target/rerun.txt", "json:target/cucumber.json"},
         features = "src/test/resources/features",
         glue = "steps",
-         tags = "@addDriver",
+         tags = "@createAddressAPI",
         dryRun = false
 )
 public class Runner {
 }
+
 
 
 
